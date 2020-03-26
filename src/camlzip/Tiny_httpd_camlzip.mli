9d6f4d25cfb89c82ee07e39fdd519fb0dec68b5a
Simon Cruanes <simon.cruanes.2007@m4x.org>
2020-03-25 22:03:33 -0400

    fix(camlzip): proper handling of transfer-encoding for `deflate`

--- src/camlzip/Tiny_httpd_camlzip.mli
+++ src/camlzip/Tiny_httpd_camlzip.mli
@@ -1,3 +1,5 @@
 
-val setup : Tiny_httpd.t -> unit
-(** Install callbacks for tiny_httpd to be able to encode/decode compressed streams *)
+val setup : ?buf_size:int -> Tiny_httpd.t -> unit
+(** Install callbacks for tiny_httpd to be able to encode/decode
+    compressed streams
+    @param buf_size size of the underlying buffer for compression/decompression *)
