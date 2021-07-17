93729b1b0a4482a5017926afdcca7f55dd778ebd
Simon Cruanes <simon.cruanes.2007@m4x.org>
2021-07-17 18:06:39 -0400

    fix

--- examples/run_test.sh
+++ examples/run_test.sh
@@ -2,9 +2,9 @@
 
 PORT=8082
 
-./sse_server.exe -- -p $PORT &
+./sse_server.exe -p $PORT &
 sleep 0.1
-./sse_client.exe -- -p $PORT --alarm=1 /count || true
+./sse_client.exe -p $PORT --alarm=1 /count || true
 
 kill %1
 echo "success"
