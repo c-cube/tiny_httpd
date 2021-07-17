0895554cb46102839c8b28326014883f07dde459
Simon Cruanes <simon.cruanes.2007@m4x.org>
2021-07-17 19:14:10 -0400

    crlf bs again

--- examples/run_test.sh
+++ examples/run_test.sh
@@ -4,7 +4,7 @@ PORT=8082
 
 ./sse_server.exe -p $PORT &
 sleep 0.1
-./sse_client.exe -p $PORT --alarm=1 /count || true
+./sse_client.exe -p $PORT --alarm=1 /count | tr -d '\r' || true
 
 kill %1
 echo "success"
