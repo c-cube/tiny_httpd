912c7b1fd70428108f82a40172fc710f0aeb5fdc
craff <christophe@raffalli.eu>
2021-12-19 00:49:09 -1000

    close fd correctly

--- tests/download_chunked.sh
+++ tests/download_chunked.sh
@@ -8,16 +8,16 @@ PID=$!
 sleep 0.1
 
 echo download1 1>&2
-curl -N "http://localhost:${PORT}/foo_50" -o data2 \
+curl -N "http://localhost:${PORT}/foo_50" -o data21 \
   -H 'Tranfer-encoding: chunked'
 
 echo download2 1>&2
-curl -N "http://localhost:${PORT}/foo_50" -o data2 \
+curl -N "http://localhost:${PORT}/foo_50" -o data22 \
   -H 'Tranfer-encoding: chunked'
 
 echo download3 1>&2
-curl -N "http://localhost:${PORT}/foo_50" -o data2 \
+curl -N "http://localhost:${PORT}/foo_50" -o data23 \
   -H 'Tranfer-encoding: chunked'
 
 kill $PID
-wc data2
+wc data21 data22 data23
