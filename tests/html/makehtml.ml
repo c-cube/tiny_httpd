5c1a7310eed588fae985c911f92ac59067beb310
Simon Cruanes <simon.cruanes.2007@m4x.org>
2022-03-16 22:27:30 -0400

    add regression test for html

---
+++ tests/html/makehtml.ml
@@ -0,0 +1,34 @@
+open Tiny_httpd_html
+let spf = Printf.sprintf
+
+let t1() =
+  html [] [
+    head [] [];
+    body [] [
+      ul [A.style "list-style: circle"] (
+        List.init 100 (fun i -> li [A.id (spf "l%d" i)] [txt (spf "item %d" i)])
+      )
+    ]
+  ]
+
+
+let t2() =
+  html [] [
+    head [] [];
+    body [] [
+      ul' [A.style "list-style: circle"] (fun buf ->
+          for i=0 to 99 do
+            li [A.id (spf "l%d" i)] [txt (spf "item %d" i)] buf
+          done
+      )
+    ]
+  ]
+
+let render t =
+  print_endline @@ to_string_top @@ t
+
+let () =
+  match Sys.argv.(1) with
+  | "1" -> render @@ t1()
+  | "2" -> render @@ t2()
+  | _ -> failwith "unknown cmd"
