82ebf85ee74673f39e1528b32c5d3d85326563bd
Simon Cruanes <simon.cruanes.2007@m4x.org>
2023-05-24 15:13:27 -0400

    remove dep on ounit2 and qtest

--- src/Tiny_httpd_util.ml
+++ src/Tiny_httpd_util.ml
@@ -1,13 +1,3 @@
-(* test utils *)
-(*$inject
-  let pp_res f = function Ok x -> f x | Error e -> e
-  let pp_res_query = (Q.Print.(pp_res (list (pair string string))))
-  let err_map f = function Ok x-> Ok (f x) | Error e -> Error e
-  let sort_l l = List.sort compare l
-  let eq_sorted a b = (=) (err_map sort_l a)(err_map sort_l b)
-  let is_ascii_char c = Char.code c < 128
-*)
-
 let percent_encode ?(skip = fun _ -> false) s =
   let buf = Buffer.create (String.length s) in
   String.iter
@@ -20,16 +10,6 @@ let percent_encode ?(skip = fun _ -> false) s =
       | c -> Buffer.add_char buf c)
     s;
   Buffer.contents buf
-
-(*$= & ~printer:(fun s->s)
-  "hello%20world" (percent_encode "hello world")
-  "%23%25^%24%40^%40" (percent_encode "#%^$@^@")
-  "a%20ohm%2B5235%25%26%40%23%20---%20_" (percent_encode "a ohm+5235%&@# --- _")
-*)
-
-(*$= & ~printer:Q.(Print.(option string))
-  (Some "?") (percent_decode @@ percent_encode "?")
-*)
 
 let hex_int (s : string) : int = Scanf.sscanf s "%x" (fun x -> x)
 
@@ -56,14 +36,6 @@ let percent_decode (s : string) : _ option =
     done;
     Some (Buffer.contents buf)
   with Exit -> None
-
-(*$QR & ~count:1_000 ~long_factor:20
-    Q.string (fun s ->
-        String.iter (fun c -> Q.assume @@ is_ascii_char c) s;
-        match percent_decode (percent_encode s) with
-        | Some s' -> s=s'
-        | None -> Q.Test.fail_report "invalid percent encoding")
-*)
 
 exception Invalid_query
 
@@ -95,16 +67,6 @@ let split_on_slash s : _ list =
       i := j + 1
   done;
   List.rev !l
-
-(*$= & ~printer:Q.Print.(list string)
-  ["a"; "b"] (split_on_slash "/a/b")
-  ["coucou"; "lol"] (split_on_slash "/coucou/lol")
-  ["a"; "b"; "c"] (split_on_slash "/a/b//c/")
-  ["a"; "b"] (split_on_slash "//a/b/")
-  ["a"] (split_on_slash "/a//")
-  [] (split_on_slash "/")
-  [] (split_on_slash "//")
-*)
 
 let parse_query s : (_ list, string) result =
   let pairs = ref [] in
@@ -145,20 +107,3 @@ let parse_query s : (_ list, string) result =
   | Invalid_argument _ | Not_found | Failure _ ->
     Error (Printf.sprintf "error in parse_query for %S: i=%d,j=%d" s !i !j)
   | Invalid_query -> Error ("invalid query string: " ^ s)
-
-(*$= & ~printer:pp_res_query ~cmp:eq_sorted
-  (Ok ["a", "b"; "c", "d"]) (parse_query "a=b&c=d")
-*)
-
-(*$QR & ~long_factor:20 ~count:1_000
-    Q.(small_list (pair string string))
-      (fun l ->
-        List.iter (fun (a,b) ->
-            Q.assume (a<>"" && b<>"" );
-            String.iter (fun c -> Q.assume @@ is_ascii_char c) a;
-            String.iter (fun c -> Q.assume @@ is_ascii_char c) b;
-          ) l;
-        let s = String.concat "&"
-            (List.map (fun (x,y) -> percent_encode x ^"="^percent_encode y) l) in
-        eq_sorted (Ok l) (parse_query s))
-*)
