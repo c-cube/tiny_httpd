bc4bce8083ee7455e4f00a44a1efa9710367cb75
Simon Cruanes <simon.cruanes.2007@m4x.org>
2023-06-26 16:05:48 -0400

    doc

--- src/eio/tiny_httpd_eio.mli
+++ src/eio/tiny_httpd_eio.mli
@@ -1,3 +1,12 @@
+(** Tiny httpd EIO backend.
+
+    This replaces the threads + Unix blocking syscalls of {!Tiny_httpd_server}
+    with an Eio-based cooperative system.
+
+    {b NOTE}: this is very experimental and will absolutely change over time,
+      especially since Eio itself is also subject to change.
+    @since NEXT_RELEASE *)
+
 (* TODO: pass in a switch *)
 
 type 'a with_args =
