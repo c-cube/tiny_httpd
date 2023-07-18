4a78eeb69ce9ee14cbe45a67a5a251307396bf1b
Simon Cruanes <simon.cruanes.2007@m4x.org>
2023-07-18 14:14:22 -0400

    html: ability to write a html document to an IO.Out_channel.t

--- src/Tiny_httpd_html.ml
+++ src/Tiny_httpd_html.ml
@@ -6,32 +6,49 @@
     @since 0.12
 *)
 
+module IO = Tiny_httpd_io
+
 include Tiny_httpd_html_
 (** @inline *)
+
+(** Write an HTML element to this out channel.
+    @param top if true, add DOCTYPE at the beginning. The top element should then
+    be a "html" tag.
+    @since NEXT_RELEASE
+    *)
+let to_out_channel ?(top = false) (self : elt) (out : IO.Out_channel.t) : unit =
+  let out = Out.create_of_out out in
+  if top then Out.add_string out "<!DOCTYPE html>\n";
+  self out
 
 (** Convert a HTML element to a string.
     @param top if true, add DOCTYPE at the beginning. The top element should then
     be a "html" tag. *)
-let to_string ?(top = false) (self : elt) : string =
-  let out = Out.create () in
-  if top then Out.add_string out "<!DOCTYPE html>\n";
-  self out;
-  Out.to_string out
+let to_string ?top (self : elt) : string =
+  let buf = Buffer.create 64 in
+  let out = IO.Out_channel.of_buffer buf in
+  to_out_channel ?top self out;
+  Buffer.contents buf
 
 (** Convert a list of HTML elements to a string.
     This is designed for fragments of HTML that are to be injected inside
     a bigger context, as it's invalid to have multiple elements at the toplevel
     of a HTML document. *)
 let to_string_l (l : elt list) =
-  let out = Out.create () in
+  let buf = Buffer.create 64 in
+  let out = Out.create_of_buffer buf in
   List.iter
     (fun f ->
       f out;
       Out.add_format_nl out)
     l;
-  Out.to_string out
+  Buffer.contents buf
 
 let to_string_top = to_string ~top:true
+
+(** Write a toplevel element to an output channel.
+    @since NEXT_RELEASE *)
+let to_out_channel_top = to_out_channel ~top:true
 
 (** Convert a HTML element to a stream. This might just convert
     it to a string first, do not assume it to be more efficient. *)
