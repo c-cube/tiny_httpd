d9b3731207c956ebe6ca897a630097bcaa52b654
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-02-21 22:03:31 -0500

    feat: optional dep on magic-mime for http_of_dir

--- src/Tiny_httpd_dir.ml
+++ src/Tiny_httpd_dir.ml
@@ -310,18 +310,8 @@ let add_vfs_ ~on_fs ~top ~config ~vfs:((module VFS : VFS) as vfs) ~prefix server
                 [ "Content-Type", "text/javascript" ]
               else if on_fs then (
                 (* call "file" util *)
-                try
-                  let p =
-                    Unix.open_process_in
-                      (Printf.sprintf "file -i -b %S" (top // path))
-                  in
-                  finally_
-                    ~h:(fun p -> ignore @@ Unix.close_process_in p)
-                    p
-                    (fun p ->
-                      try [ "Content-Type", String.trim (input_line p) ]
-                      with _ -> [])
-                with _ -> []
+                let ty = Mime_.mime_of_path (top // path) in
+                [ "content-type", ty ]
               ) else
                 []
             in
